55da19647020893a42ab777e2b82b314cc91b5cd
zhoujing <zhoujing@gridsum.com>
2019-04-19 11:25:41 +0800

    delete files

--- java/spring-demo-proj/web/src/main/java/us/gfzj/web/constants/ConfKeyTemplate.java
+++
@@ -1,9 +0,0 @@
-package us.gfzj.web.constants;
-
-/**
- * @author zhoujing
- * Create on 19-2-1
- */
-public class ConfKeyTemplate {
-    public static final String INDEX_NAME_FORMAT = "%s.es.index.name.format";
-}
